e91a7e29e42200e98fcf87732f7a00e57ca403ad
InFlamIty <107593393+InFlamIty@users.noreply.github.com>
2023-05-22 18:20:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+img = ""
+function setup()
+{
+    canvas = createCanvas(800 , 600)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+}
+resultsArray = []
+function draw()
+{
+    image(video , 0 , 0 , 800 , 600)
+    if (status != "") {
+        objectDetector.detect(canvas , gotResults)
+        for (let i = 0; i < resultsArray.length; i++) {
+            r = random(255)
+            g = random(255)
+            b = random(255)
+            document.getElementById("status").innerHTML = "Object Detected"
+            fill(r , g , b)
+            textSize(25)
+            con = floor(resultsArray[i].confidence * 100)
+            text(resultsArray[i].label + con + "%" , resultsArray[i].x , resultsArray[i].y)
+            noFill()
+            stroke(r , g , b)
+            rect(resultsArray[i].x , resultsArray[i].y , resultsArray[i].width , resultsArray[i].height)
+            document.getElementById("itemNum").innerHTML = "Amount of Items = " + resultsArray.length
+        }
+    }
+}
+function preload()
+{
+    img = loadImage("idk.jpg")
+}
+function modelLoaded()
+{
+    console.log("this should be working and not creating virus")
+    status = true
+}
+status = ""
+function gotResults(error , results)
+{
+    if (error) {
+        console.log(error , "i guess its not working :(")
+    }
+    console.log(results , "if you see this it worked i guess :)")
+    resultsArray = results
+}
+function starter()
+{
+    objectDetector = ml5.objectDetector("cocossd" , modelLoaded)
+    document.getElementById("status").innerHTML = "detecting objects ...."
+}
